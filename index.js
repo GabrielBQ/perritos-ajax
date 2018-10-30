c47d8e0f2d45a9aca7dfe2119528991c67131f90
Gabriel B Quintáns <gabrielbarreiroquintans@gmail.com>
2018-10-30 13:50:35 +0100

    Simplificado Todo(guardada explicacion en codePen)

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-//Nombramos la URL de la página para poder usarla más facilmente
-const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
+document.querySelector(".add-perretes").addEventListener("click", onClick);
 
-//fetch recibe un String (la URL en este caso) y  nos devuelve una promesa(el return de .then)
-const dogPromise = fetch(RANDOM_DOG_URL);
-
-//then es una función(que recibe una función) que te permite acceder a  una promesa (hace una llamada a AJAX para poder recibir DESPUÉS la imagen)
-dogPromise
-  .then(function(apiResponse) {
-    //.json no recibe nada y nos devuelve una promesa ( es un objeto de JavaScript)
-    return apiResponse.json();
-  })
-  .then(function(jsonResponse) {
-    console.log(jsonResponse.message); // aquí es donde se encuentra la URL de la imagen
-    const img = document.createElement("img");//creamos la imagen
-    console.log(img);
-    img alt = "Perrito Bonito!";//dar la parte alternativa a la foto
-    console.log(img);
-    img src = jsonResponse.message;//es la direccion de la imagen va a estar aquí
-    console.log(img);
-    const container = document.querySelector(".perretes")
-    container.appendChild(img); //coge src y alt y los mete dentro del div que le mandes antes del punto
-  });
-//De esta forma ya casi estaría, solo nos falta añadir la funcion onClick para que al cliquear aparezca la imagen
-//Así:
-const button = document.querySelector("add-perretes");//nombramos el botón(o no)
-button.addEventListener("click", onClick);//(le añadimos el evento)
-
-function onClick(_event){     // metemos todo dentro de la función
-fetch(RANDOM_DOG_URL)
-.then(function (apiResponse {
-  return apiResponse.json();
-})
-  .then(function(jsonResponse) {
-    const img = document.createElement("img");
-    img.alt = "Perrito Bonito!";
-    img.src = jsonResponse.message;
-
-    document.querySelector(".perretes").appendChild(img);
-  });
+function onClick() {
+  const randomDogUrl = "https://dog.ceo/api/breeds/image/random";
+  fetch(randomDogUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response2) {
+      const img = document.createElement("img");
+      img.alt = "Un Lindo Perrito";
+      img.src = response2.message;
+      document.querySelector(".perretes").appendChild(img);
+    });
 }
