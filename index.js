85a3a192aeecfb268eb6e6cd99aec9a7309a8915
Gabriel B Quintáns <gabrielbarreiroquintans@gmail.com>
2018-10-31 14:54:16 +0100

    Add Option2/add function capitalizeFirstLetter

--- index.js
+++ index.js
@@ -28,13 +28,22 @@ function onClickBreeds(_event) {
       label.htmlFor = "breed-select";
       label.innerText = "🐶 Escoge una Raza 🐶 ";
       document.querySelector(".breed-container").appendChild(label);
+      //TO DO: function createSelect
       const select = document.createElement("select");
       select.id = "breed-select";
       document.querySelector(".breed-container").appendChild(select);
+      //TO DO: function createOption
       const option = document.createElement("option");
       option.value = "";
       option.innerText = "🐺Coge una Raza🐺";
       document.querySelector("#breed-select").appendChild(option);
+      //TO DO: function createOption2
+      const razas = Object.keys(breeds);
+      console.log(razas[26]);
+      const option2 = document.createElement("option");
+      option2.value = razas[26];
+      option2.innerText = capitalizeFirstLetter(razas[26]);
+      document.querySelector("#breed-select").appendChild(option2);
     });
   /*<label for="breed-select">🐶 Escoge una Raza 🐶 </label>
     <p></p><select class="breeds-select">
@@ -43,7 +52,6 @@ function onClickBreeds(_event) {
       <option value="airedale">airedale</option>
     </select>*/
 }
-s;
 
 function toJson(apiResponse) {
   return apiResponse.json();
@@ -59,4 +67,8 @@ function makeImageFrom(dogUrl) {
 function toJsonResponse(jsonResponse) {
   const image = makeImageFrom(jsonResponse.message);
   document.querySelector(".perretes").appendChild(image);
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
